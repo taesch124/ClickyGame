a4cc17d9f49aab6ad3a2316532f45c8c59540222
taesch124 <taesch124@gmail.com>
2019-02-02 14:30:16 -0500

    Updates

---
+++ precache-manifest.b976f2b920fadffb01d02a1c0a4fda70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "85ed4bafba1fdb019440",
+    "url": "/ClickyGame/static/js/main.85ed4baf.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/ClickyGame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "85ed4bafba1fdb019440",
+    "url": "/ClickyGame/static/css/main.1fc858fc.chunk.css"
+  },
+  {
+    "revision": "677dd2badf5f1bc041d97444cc648306",
+    "url": "/ClickyGame/index.html"
+  }
+];
